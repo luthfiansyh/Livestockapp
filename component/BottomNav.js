f38391a5e334c4b244cc9f319c9a56630d6b23fc
unknown <muhammadluthfiansyah15@gmail.com>
2021-09-25 23:06:11 +0700

    install react navigation, icons and svg

--- component/BottomNav.js
+++ component/BottomNav.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {
   Text,
   View,
+  Pressable
 } from 'react-native';
+
+import Icon from 'react-native-ico-material-design';
 
 
 const BottomNav = () =>{
   return(
     <View style={{height: 54, backgroundColor: '#CCC', flexDirection: 'row'}}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',}}>
-        <View style={{width: 24, height: 24, backgroundColor: '#545454'}}></View>
+        <View style={{width: 24, height: 24, backgroundColor: '#545454'}}>
+          <Icon name="add-plus-button" height="24" width="24" />
+        </View>
         <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Beranda</Text>
       </View>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',}}>
