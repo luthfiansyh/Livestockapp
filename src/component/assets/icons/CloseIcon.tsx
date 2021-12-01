4574474ff7aea571792a94e97419bc976b00ed80
unknown <muhammadluthfiansyah15@gmail.com>
2021-12-01 10:51:14 +0700

    adding icons and fonts

---
+++ src/component/assets/icons/CloseIcon.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Svg, {Path} from 'react-native-svg';
+
+const CloseIcon = () => {
+    return(
+        <Svg width="24" height="24" viewBox="0 0 24 24">
+            <Path d="M18.0014 4L12 10L6 4L4 6L10 12L4 18L6 20L12 14L18.0014 20L20.0014 18L14.0014 12L20.0014 6L18.0014 4Z" fill="#626262"/>
+        </Svg>
+
+    )
+}
+
+export default CloseIcon;
