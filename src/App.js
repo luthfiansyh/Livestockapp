c6f0d9bc551ec1b6b22d4c2cd8372f49ff5d198b
unknown <muhammadluthfiansyah15@gmail.com>
2021-12-20 08:02:32 +0700

    build stack navigators

--- src/App.js
+++ src/App.js
@@ -8,32 +8,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Router from './router';
 import auth from '@react-native-firebase/auth';
 import { AuthContext } from './router/AuthProvider';
+import LoginProvider from './utils/LoginProvider';
+import AppStack from './utils/AppStack';
 
 const App = ()  =>{
 
-  // const (user, setUser) = useContext(AuthContext);
-
-  // const (initializing, setInitializing) = useState(true);
-
-  // const onAuthStateChanged = (user) => {
-  //   setUser(user);
-  //   if(initializing) setInitializing(false);
-  // }
-
-  // useEffect(() => {
-  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-  //   return subscriber;
-
-  // }, []);
-
-  // if(initializing) return null;
-
-
   return(
     <View style={{flex:1}}>
-      <NavigationContainer>
-        <Router/>
-      </NavigationContainer>
+      <LoginProvider>
+        <AppStack/>
+      </LoginProvider>
     </View>
 
   )
